55b8889c1314a3723748fd772cc1bc4890d72ce6
Dany Christian <45038103+MSerperior@users.noreply.github.com>
2022-12-02 07:03:36 +0700

    update 0212

--- test-selenium.py
+++ test-selenium.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 import time
 import random
 from bs4 import BeautifulSoup
-
+import json
 
 def wasteTime(timeToWait):
     time.sleep(timeToWait+random.random()*3)
@@ -51,6 +51,16 @@ def input_date(date):
 # file = open("test.html", "w")
 # file.write(html)
 
+data = dict()
+
+data['10/10/2022'] = {
+    'Top Buyer': {},
+    'Top Seller': {},
+    'T. Val': 0,
+    'F. NVal': 0,
+    'T. Lot': 0,
+    'Avg': 0,
+}
 
 def read_html():
     file = open("test.html", "r")
@@ -65,31 +75,48 @@ def get_data(html):
     thead = table.find('thead')
     headers = thead.find_all('th')
     headers = [ele.text.strip() for ele in headers]
-    print(headers)
+    # print(headers)
 
     tbody = table.find('tbody')
     rows = tbody.find_all('tr')
     for row in rows:
         cols = row.find_all('td')
+
         cols = [ele.text.strip() for ele in cols]
-        print(cols)
+        buyer = dict()
+        buyer["Buyer"] = cols[0]
+        buyer["B.Lot"] = money_to_int(cols[1])
+        buyer["B.Val"] = money_to_int(cols[2])
+        buyer["B.Avg"] = money_to_int(cols[3])
+        seller = dict()
+        seller["Seller"] = cols[5]
+        seller["S.Lot"] = money_to_int(cols[6])
+        seller["S.Val"] = money_to_int(cols[7])
+        seller["S.Avg"] = money_to_int(cols[8])
+
+
+        data["10/10/2022"]["Top Buyer"].setdefault(int(cols[4]), buyer)
+        data["10/10/2022"]["Top Seller"].setdefault(int(cols[4]), seller)
+        # print(cols)
 
     tfoot = table.find('tfoot')
     foot_rows = tfoot.find_all('span')
     foot_rows = [ele.text.strip() for ele in foot_rows]
-    print(foot_rows)
+    # print(foot_rows)
 
+    data["10/10/2022"]["T. Val"] = money_to_int(foot_rows[0].split(' : ')[-1])
+    data["10/10/2022"]["F. NVal"] = money_to_int(foot_rows[1].split(' : ')[-1])
+    data["10/10/2022"]["T. Lot"] = money_to_int(foot_rows[2].split(' : ')[-1])
+    data["10/10/2022"]["Avg"] = money_to_int(foot_rows[3].split(' : ')[-1])
+    print(json.dumps(data, indent=2))
+# print(float("35,661".replace(',', '')))
 
-data = dict()
-
-data['10/10/2022'] = {
-    'Top Buyer': {},
-    'Top Seller': {},
-    'T. Val': 0,
-    'F. NVal': 0,
-    'T. Lot': 0,
-    'Avg': 0,
-}
-
-
+def money_to_int(money):
+    if(money[-1] == 'M'):
+        return int(float(money[:-1].replace(',', ''))*1000000)
+    elif(money[-1] == 'B'):
+        return int(float(money[:-1].replace(',', ''))*1000000000)
+    return int(money.replace(',', ''))
+    
 get_data(read_html())
+# print(money_to_int("96.5 M"))
